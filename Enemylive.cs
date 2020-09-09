3482acb8ae55fb42a4c9d26f78724d692d42b664
Castrillon-San <60115630+Castrillon-San@users.noreply.github.com>
2020-09-09 17:57:35 -0500

    Add files via upload

---
+++ Enemylive.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemylive : MonoBehaviour
+{
+    private int life = 5;
+    EnemyWaves wavesCode;
+    private GameObject GameController;
+
+    private void Start()
+    {
+        GameController = GameObject.Find("GAME CONTROLLER");
+        wavesCode = GameController.GetComponent<EnemyWaves>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            GetDamage(10);
+        }
+    }
+
+    public void GetDamage(int damage)
+    {
+        life = life - damage;
+        if (life<=0)
+        {
+            //Animación de muerte
+            Destroy(gameObject);
+
+            wavesCode.AliveCount();
+            
+            /*if(wavesCode.currentAliveEnemies<=0)
+            {
+                StartCoroutine(wavesCode.WaveWaiter());
+            }*/
+        }
+    }
+
+}
